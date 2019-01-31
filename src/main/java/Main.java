ac0df78a7a8e2ba653a6c7005879d39c887ae47d
Matthew Oberg <matthewroberg@gmail.com>
2019-01-31 15:46:08 -0800

    Nicer Looking Code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -168,51 +168,28 @@ public final class Main {
 			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		}
 
-		/**
-		* This is the primary method that runs the entire pipeline and updates the outputs.
-		*/
 		@Override
 		public void process(Mat source0) {
-			// Step HSV_Threshold0:
 			Mat hsvThresholdInput = source0;
 			double[] hsvThresholdHue = {74.46043165467626, 120.1023890784983};
 			double[] hsvThresholdSaturation = {32.10431654676259, 76.58703071672356};
 			double[] hsvThresholdValue = {210.97122302158272, 255.0};
 			hsvThreshold(hsvThresholdInput, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, hsvThresholdOutput);
 
-			// Step Find_Contours0:
 			Mat findContoursInput = hsvThresholdOutput;
 			boolean findContoursExternalOnly = false;
 			findContours(findContoursInput, findContoursExternalOnly, findContoursOutput);
 
 		}
 
-		/**
-		* This method is a generated getter for the output of a HSV_Threshold.
-		* @return Mat output from HSV_Threshold.
-		*/
 		public Mat hsvThresholdOutput() {
 			return hsvThresholdOutput;
 		}
 
-		/**
-		* This method is a generated getter for the output of a Find_Contours.
-		* @return ArrayList<MatOfPoint> output from Find_Contours.
-		*/
 		public ArrayList<MatOfPoint> findContoursOutput() {
 			return findContoursOutput;
 		}
 
-
-		/**
-		* Segment an image based on hue, saturation, and value ranges.
-		*
-		* @param input The image on which to perform the HSL threshold.
-		* @param hue The min and max hue
-		* @param sat The min and max saturation
-		* @param val The min and max value
-		* @param output The image in which to store the output.
-		*/
 		private void hsvThreshold(Mat input, double[] hue, double[] sat, double[] val,
 			Mat out) {
 			Imgproc.cvtColor(input, out, Imgproc.COLOR_BGR2HSV);
@@ -220,13 +197,6 @@ public final class Main {
 			new Scalar(hue[1], sat[1], val[1]), out);
 		}
 
-		/**
-		* Sets the values of pixels in a binary image to their distance to the nearest black pixel.
-		* @param input The image on which to perform the Distance Transform.
-		* @param type The Transform.
-		* @param maskSize the size of the mask.
-		* @param output The image in which to store the output.
-		*/
 		private void findContours(Mat input, boolean externalOnly,
 			List<MatOfPoint> contours) {
 			Mat hierarchy = new Mat();
@@ -277,5 +247,5 @@ public final class Main {
 				return;
 			}
 		}
-  }
+	}
 }
