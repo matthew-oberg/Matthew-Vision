63d2fb366a2d7191b2fda18fbca56090e3961fb9
Matthew Oberg <matthewroberg@gmail.com>
2019-02-23 12:59:07 -0800

    Spacing Fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -52,7 +52,7 @@ public final class Main {
 		JsonElement nameElement = config.get("name");
 		if (nameElement == null) {
 			parseError("could not read camera name");
-		return false;
+			return false;
 		}
 		cam.name = nameElement.getAsString();
 
@@ -83,8 +83,8 @@ public final class Main {
 		}
 
 		if (!top.isJsonObject()) {
-		parseError("must be JSON object");
-		return false;
+			parseError("must be JSON object");
+			return false;
 		}
 		
 		JsonObject obj = top.getAsJsonObject();
@@ -111,8 +111,8 @@ public final class Main {
 		JsonElement camerasElement = obj.get("cameras");
 		
 		if (camerasElement == null) {
-		parseError("could not read cameras");
-		return false;
+			parseError("could not read cameras");
+			return false;
 		}
 		
 		JsonArray cameras = camerasElement.getAsJsonArray();
@@ -121,7 +121,6 @@ public final class Main {
 				return false;
 			}
 		}
-
 		return true;
 	}
 
@@ -151,6 +150,10 @@ public final class Main {
 
 		static {
 			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		}
+
+		public Mat hsvThresholdOutput() {
+			return hsvThresholdOutput;
 		}
 
 		@Override
@@ -185,51 +188,19 @@ public final class Main {
 
 		}
 
-		/**
-		 * This method is a generated getter for the output of a HSV_Threshold.
-		 * @return Mat output from HSV_Threshold.
-		 */
-		public Mat hsvThresholdOutput() {
-			return hsvThresholdOutput;
-		}
-
-		/**
-		 * This method is a generated getter for the output of a Find_Contours.
-		 * @return ArrayList<MatOfPoint> output from Find_Contours.
-		 */
 		public ArrayList<MatOfPoint> findContoursOutput() {
 			return findContoursOutput;
 		}
 
-		/**
-		 * This method is a generated getter for the output of a Filter_Contours.
-		 * @return ArrayList<MatOfPoint> output from Filter_Contours.
-		 */
 		public ArrayList<MatOfPoint> filterContoursOutput() {
 			return filterContoursOutput;
 		}
 
-		/**
-		 * Segment an image based on hue, saturation, and value ranges.
-		 *
-		 * @param input The image on which to perform the HSL threshold.
-		 * @param hue The min and max hue
-		 * @param sat The min and max saturation
-		 * @param val The min and max value
-		 * @param output The image in which to store the output.
-		 */
 		private void hsvThreshold(Mat input, double[] hue, double[] sat, double[] val, Mat out) {
 			Imgproc.cvtColor(input, out, Imgproc.COLOR_BGR2HSV);
 			Core.inRange(out, new Scalar(hue[0], sat[0], val[0]), new Scalar(hue[1], sat[1], val[1]), out);
 		}
 
-		/**
-		 * Sets the values of pixels in a binary image to their distance to the nearest black pixel.
-		 * @param input The image on which to perform the Distance Transform.
-		 * @param type The Transform.
-		 * @param maskSize the size of the mask.
-		 * @param output The image in which to store the output.
-		 */
 		private void findContours(Mat input, boolean externalOnly, List<MatOfPoint> contours) {
 			Mat hierarchy = new Mat();
 			contours.clear();
@@ -243,22 +214,6 @@ public final class Main {
 			Imgproc.findContours(input, contours, hierarchy, mode, method);
 		}
 
-		/**
-		 * Filters out contours that do not meet certain criteria.
-		 * @param inputContours is the input list of contours
-		 * @param output is the the output list of contours
-		 * @param minArea is the minimum area of a contour that will be kept
-		 * @param minPerimeter is the minimum perimeter of a contour that will be kept
-		 * @param minWidth minimum width of a contour
-		 * @param maxWidth maximum width
-		 * @param minHeight minimum height
-		 * @param maxHeight maximimum height
-		 * @param Solidity the minimum and maximum solidity of a contour
-		 * @param minVertexCount minimum vertex Count of the contours
-		 * @param maxVertexCount maximum vertex Count
-		 * @param minRatio minimum ratio of width to height
-		 * @param maxRatio maximum ratio of width to height
-		 */
 		private void filterContours(List<MatOfPoint> inputContours, double minArea, double minPerimeter, double minWidth, double maxWidth, double minHeight, double maxHeight, double[] solidity, double maxVertexCount, double minVertexCount, double minRatio, double maxRatio, List<MatOfPoint> output) {
 			final MatOfInt hull = new MatOfInt();
 			output.clear();
@@ -280,7 +235,7 @@ public final class Main {
 				mopHull.create((int) hull.size().height, 1, CvType.CV_32SC2);
 				for (int j = 0; j < hull.size().height; j++) {
 					int index = (int)hull.get(j, 0)[0];
-					double[] point = new double[] { contour.get(index, 0)[0], contour.get(index, 0)[1]};
+					double[] point = new double[] {contour.get(index, 0)[0], contour.get(index, 0)[1]};
 					mopHull.put(j, 0, point);
 				}
 				final double solid = 100 * area / Imgproc.contourArea(mopHull);
@@ -295,7 +250,6 @@ public final class Main {
 			}
 		}
 	}
-
 
 	private static class Contour implements Comparable<Contour> {
 	    private Direction direction;
@@ -367,11 +321,10 @@ public final class Main {
 		if (cameras.size() >= 2) { // both cameras exist
             VisionThread visionThread = new VisionThread(cameras.get(1), new DetectDouble(), pipeline -> {
 
-                ArrayList<Contour> contours = pipeline.filterContoursOutput().stream()
-                        .map(mat -> {
+            	ArrayList<Contour> contours = pipeline.filterContoursOutput().stream().map(mat -> {
                             Rect rect = Imgproc.boundingRect(mat);
                             return new Contour(getDirection(mat), new Point(rect.x + rect.width / 2.0, rect.y + rect.height / 2.0));
-                        }).sorted().collect(Collectors.toCollection(ArrayList::new));
+                }).sorted().collect(Collectors.toCollection(ArrayList::new));
 
                 String[] directions = contours.stream().map(c -> c.direction.toString()).collect(Collectors.toList()).toArray(new String[contours.size()]);
                 String d = "";
@@ -411,10 +364,6 @@ public final class Main {
                         hatchOneX.setDouble(pair.right.center.x);
                         hatchOneY.setDouble(pair.right.center.y);
                     }
-                    //72, 254
-                    //122, 208
-
-
                     //x: (0, 320)
                 } else {
                     synchronized (imgLock) {
